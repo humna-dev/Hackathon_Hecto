aacacd7006967d083331d80aed214c90fcae9464
Humna <hamnaallauddin4@gmail.com>
2024-12-17 13:16:28 +0500

    Refactor: Clean up class names and fix image paths

--- components/FeaturedProducts/FeaturedProducts.tsx
+++ components/FeaturedProducts/FeaturedProducts.tsx
@@ -2,64 +2,72 @@ import styles from "./FeaturedProducts.module.css";
 import Image from "next/image";
 
 const FeaturedProducts = () => {
-    return (
-      <div className={styles._main_containar}>
-        <div className={styles._containar}>
-          <div className={styles._main_heading_div}>
-            <h1 className={styles._main_heading1}>Featured Products</h1>
+  return (
+    <div className={styles._main_containar}>
+      <div className={styles._containar}>
+        {/* Heading Section */}
+        <div className={styles._main_heading_div}>
+          <h1 className={styles._main_heading1}>Featured Products</h1>
+        </div>
+
+        {/* Grid Section */}
+        <div className={styles._grid}>
+          {/* Product 1 */}
+          <div className={styles._card}>
+            <div className={styles._card_img_box}>
+              <Image
+                src="/images/file-five/component-3.png"
+                alt="Comfort Handly Craft"
+                width={180}
+                height={100}
+                priority
+              />
+            </div>
           </div>
-  
-          <div className={styles._grid}>
-            <div className={styles._card}>
-              <div className={styles._card_img_box}>
-                <Image
-                  src="/images/file-five/Component 3.png"
-                  alt="Comfort Handly Craft"
-                  width={180}
-                  height={100}
-                />
-              </div>
+
+          {/* Product 2 */}
+          <div className={styles._card}>
+            <div className={styles._card_img_box}>
+              <Image
+                src="/images/file-five/component-4.png"
+                alt="Comfort Handly Craft"
+                width={180}
+                height={100}
+                priority
+              />
             </div>
-  
-            <div className={styles._card}>
-              <div className={styles._card_img_box}>
-                <Image
-                  src="/images/file-five/Component 4.png"
-                  alt="Comfort Handly Craft"
-                  width={180}
-                  height={100}
-                />
-              </div>
+          </div>
+
+          {/* Product 3 */}
+          <div className={styles._card}>
+            <div className={styles._card_img_box}>
+              <Image
+                src="/images/file-five/featured-product-1.png"
+                alt="Comfort Handly Craft"
+                width={180}
+                height={100}
+                priority
+              />
             </div>
-  
-            <div className={styles._card}>
-              <div className={styles._card_img_box}>
-                <Image
-                  src="/images/file-five/Featured product (1).png"
-                  alt="Comfort Handly Craft"
-                  width={180}
-                  height={100}
-                />
-              </div>
+          </div>
+
+          {/* Product 4 */}
+          <div className={styles._card}>
+            <div className={styles._card_img_box}>
+              <Image
+                src="/images/file-five/featured-product.png"
+                alt="Comfort Handly Craft"
+                width={180}
+                height={100}
+                priority
+              />
             </div>
-  
-            <div className={styles._card}>
-              <div className={styles._card_img_box}>
-                <Image
-                  src="/images/file-five/Featured product.png"
-                  alt="Comfort Handly Craft"
-                  width={180}
-                  height={100}
-                />
-              </div>
-            </div>
-  
-      
           </div>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 export default FeaturedProducts;
+git
